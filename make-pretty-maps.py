5498486164c4205e3f47559d7e270963e2f5338e
PeridexisErrant <PeridexisErrant@gmail.com>
2015-01-18 21:50:06 +1100

    Initial commit
    The start of an experimental project to implement the FMM and SMM
    scripts for GIMP entirely in Python, using PIL/Pillow

---
+++ make-pretty-maps.py
@@ -0,0 +1,55 @@
+
+import PIL, os, glob
+
+"""Make pretty maps - fantasy and satellite style - from DF exports.
+
+Currently using some simplifying assumptions:
+    * Exported maps, base images, and this script are in same dir.
+"""
+
+def get_region_info():
+    """Returns a tuple of strings for an available region and date.
+    Eg: ('region1', '00250-01-01')
+
+    Taken from legends processor & in PyLNP and slightly modified
+    """
+    globbed = glob.glob('region*-*.png') + glob.glob('region*-*.bmp')
+    if globbed:
+        fname = os.path.basename(globbed[0])
+        idx = fname.index('-')
+        return (fname[:idx], fname[idx+1:idx+12])
+    raise RuntimeError('No maps available.')
+
+def get_png_maps():
+    """Returns dict of map IDs as keys and filenames are values.
+    Bitmaps are silently compressed to .png"""
+    maps = ('elw', 'el', 'veg', 'vol', 'tmp', 'bm')
+    fnames = {}
+    for k in maps:
+        m = glob.glob('-'.join(get_region_info()) + '-' + k + '.???')
+        if m and m[0].endswith('.png'):
+            fnames[k] = m[0]
+        elif m and m[0].endswith('.bmp'):
+            Image.open(m[0]).save(m[0].replace('.bmp', '.png'),
+                                  format='PNG', optimize=True)
+            os.remove(m[0])
+            fnames[k] = m[0].replace('.bmp', '.png')
+    if len(fnames) < len(maps):
+        raise RuntimeError('Insufficient maps available.')
+    return fnames
+
+def get_base_images():
+    """Returns a dict of base pics short names and paths."""
+    return {'dirt'='f_dirt.png',
+            'mountains'='f_mountains.png',
+            'trees'='f_trees.png'}
+
+def make_fantasy_map():
+    """Makes the fantasy map - a work in progress."""
+    maps = get_png_maps()
+    pics = get_base_images()
+
+    # images will be a dict of Image objects, same keys as the above
+    images = {}
+
+    # TODO:  process images
---
+++ readme.md
@@ -0,0 +1,14 @@
+DF Map Processing
+=================
+
+A personal project to mess around with processing DF's exported maps into
+interesting forms.
+
+I'm currently focussed on improving the usability of the Fantasy Map Maker
+and the Satellite Map Maker - both use GIMP in a very similar way.  I'll
+first try to make the experience easier, then understand what the script is
+doing and try to re-implement in Python with PIL, for eventual integration in
+the PyLNP.
+
+Links:  [FMM](http://www.bay12forums.com/smf/index.php?topic=73095.msg2261106#
+msg2261106), [SMM](http://www.bay12forums.com/smf/index.php?topic=137076).
